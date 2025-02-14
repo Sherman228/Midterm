3580e4dbf1fbd6c4e145cc9f7b4f3d1c85a410cb
Sherman So <so_illu@yahoo.com.hk>
2025-02-14 15:02:24 -0500

    Q4 Added

--- Midterm/NUnitCalculatorTest/UnitTest1.cs
+++ Midterm/NUnitCalculatorTest/UnitTest1.cs
@@ -4,17 +4,23 @@ using Midterm;
 
 namespace NUnitCalculatorTest
 {
-    public class Tests
+    public class UnitTest1
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        private Calculator _calculator { get; set; } = null;
 
-        [Test]
-        public void Test1()
+
+        [TestCase(-10, "The number is negative.")]
+        [TestCase(0, "The number is zero.")]
+        [TestCase(10, "The number is between 1 and 10.")]
+        [TestCase(20, "The number is between 11 and 20.")]
+        [TestCase(30, "The number is between 21 and 30.")]
+        public void CategorizeNumber_Test(int number, string sentence)
         {
-            Assert.Pass();
+            // Act
+            string result = Calculator.CategorizeNumber(number);
+
+            // Assert
+            Assert.AreEqual(sentence, result);
         }
     }
 }
