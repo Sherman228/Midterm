0f23290842712fd389ef19f796d23e7f8527217d
Sherman So <so_illu@yahoo.com.hk>
2025-02-14 14:21:52 -0500

    Calculator Test File Added

---
+++ Midterm/NUnitCalculatorTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using static Midterm.Calculator;
+using NUnit.Framework;
+using Midterm;
+
+namespace NUnitCalculatorTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
